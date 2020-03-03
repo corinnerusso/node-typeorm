830806ee293b9efdcba5ee07e56a76a56333e0ca
ryme07 <remydelrieu@hotmail.fr>
2020-03-03 11:08:56 +0100

    feat: edit config

--- src/loaders/typeorm.ts
+++ src/loaders/typeorm.ts
@@ -1,5 +1,6 @@
 import { createConnection } from "typeorm";
 import { User } from "../entity/user.entity";
+import * as ormconfig from "../ormconfig.json";
 
 // Ici on met toutes les connections avec la BDD, et on oublie pas de rajouter les diverses 'entities'
 // à l'intérieur des crochets pour les déclarer en quelque sorte.
@@ -7,10 +8,10 @@ import { User } from "../entity/user.entity";
 export default async () => {
   await createConnection({
     type: "mysql",
-    host: "localhost",
-    username: "",
-    password: "",
-    database: "",
+    host: ormconfig.host,
+    username: ormconfig.username,
+    password: ormconfig.password,
+    database: ormconfig.database,
     entities: [User],
     synchronize: true
   });
